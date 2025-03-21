300a1bbb63c876200bdc14535d9dd85bec5fe603
stefantagarski <stefantagarski@gmail.com>
2025-03-21 22:02:45 +0100

    initial aud commit

---
+++ IS_aud/EShop.Domain/DomainModels/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿using EShop.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Domain.DomainModels
+{
+    public class ShoppingCart
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? OwnerId { get; set; }
+        public EShopApplicationUser? Owner { get; set; }
+        public virtual ICollection<ProductInShoppingCart>? ProductInShoppingCarts { get; set; }
+    }
+}
---
+++ IS_aud/EShop.web/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EShop.Domain.DomainModels;
+using EShop.Domain.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<EShopApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<ProductInShoppingCart> ProductInShoppingCarts { get; set; }
+    }
+}
