25ad0dedffc077b96b1508577169aa8c42322c5e
stefantagarski <stefantagarski@gmail.com>
2025-04-03 23:22:15 +0200

    Add lab 1

---
+++ IS_lab1/Game/Game.Domain/Identity/AthletesApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Domain.Idenity
+{
+    public class AthletesApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+    }
+}
